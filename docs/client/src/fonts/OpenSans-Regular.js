6095849af24cbfbce4597f3db6a5c73df156ad39
Ra-Fil <r.filipova@email.cz>
2025-07-24 22:15:08 +0200

    Delete docs/client directory

--- docs/client/src/fonts/OpenSans-Regular.js
+++
@@ -1,3 +0,0 @@
-const OpenSansFontData = "base64_encoded_font_data_here";
-jsPDF.API.addFileToVFS("OpenSans-Regular.ttf", OpenSansFontData);
-jsPDF.API.addFont("OpenSans-Regular.ttf", "OpenSans", "regular");
